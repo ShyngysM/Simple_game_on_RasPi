df6c68c9f2e8c5496f7c81d838eaf7416aa61b8b
ShyngysM <tschingis.muratbek@gmail.com>
2022-11-11 14:49:59 +0100

    organizing

---
+++ Schritt_1/endless_runner.py
@@ -0,0 +1,84 @@
+import pygame as pg 
+import random
+
+pg.init()
+
+#game constants
+white = (255, 255, 255)
+black = (0, 0, 0)
+green = (0,255,0)
+red = (255,0,0)
+blue = (0,0,255)
+WIDTH = 450
+HEIGHT = 600
+
+#game veriables
+score = 0
+player_x = 200
+player_y = 450
+x_change = 0
+obstacles = [150, 300, 450]
+obstacle_speed = 2
+obstacle_x = 0
+active = False
+
+screen = pg.display.set_mode([WIDTH, HEIGHT])
+pg.display.set_caption('Endless Runner')
+background = black
+fps = 60
+font = pg.font.Font('freesansbold.ttf', 16)
+timer = pg.time.Clock()
+
+running = True
+# Start game
+while running:
+    timer.tick(fps)
+    screen.fill(background)
+    score_text = font.render(f'Score: {score}', True, white, black)
+    screen.blit(score_text, (10, 10))
+    player = pg.draw.rect(screen, white, [player_x, player_y, 30, 30])
+    obstacle0 = pg.draw.rect(screen, red, [obstacle_x, obstacles[0],  20, 20])
+    obstacle1 = pg.draw.rect(screen, green, [obstacle_x + 150, obstacles[1], 20, 20])
+    obstacle2 = pg.draw.rect(screen, blue, [obstacle_x + 300, obstacles[2], 20, 20])
+
+# Events -> Press buttons
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+# Change from here!
+        if event.type == pg.KEYDOWN and active:
+            if event.key == pg.K_RIGHT:
+                x_change = 2
+            if event.key == pg.K_LEFT:
+                x_change = -2
+        elif event.type == pg.KEYDOWN and not active:
+            if event.key == pg.K_RIGHT or event.key == pg.K_LEFT:
+                obstacles = [150, 300, 450]
+                player_x = 200 
+                score = 0
+                active = True
+# Change till here!
+
+# Create and handle obstacles
+    for i in range(len(obstacles)):
+        if active:
+            obstacles[i] += obstacle_speed
+            if obstacles[i] > 600:
+                obstacles[i] = random.randint(-200, 200)
+                obstacle_x = random.randint(0, 100)
+                score += 1
+            if player.colliderect(obstacle0) or player.colliderect(obstacle1) or player.colliderect(obstacle2):
+                active = False
+                x_change = 0
+
+# Player movement 
+    if 0 <= player_x <= 420:
+        player_x += x_change
+    if player_x < 0:
+        player_x = 0
+    if player_x > 420:
+        player_x = 420
+
+    pg.display.flip()
+pg.quit()
